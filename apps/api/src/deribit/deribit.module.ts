b206b7b3ff7411a9bbc32a68a6f80a74976313e6
Kampter <psbswsg@gmail.com>
2026-05-19 12:36:36 +0800

    feat(api): DeribitModule imports DatabaseModule

--- apps/api/src/deribit/deribit.module.ts
+++ apps/api/src/deribit/deribit.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from '../database/database.module';
 import { DeribitService } from './deribit.service';
 import { DeribitController } from './deribit.controller';
 
 @Module({
+  imports: [DatabaseModule],
   providers: [DeribitService],
   controllers: [DeribitController],
   exports: [DeribitService],
